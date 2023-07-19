af0aed3fc284ae4dda67751cac0b27ac7d578305
Ayush Pachpandey <ayushpachpandey09@gmail.com>
2023-07-19 16:17:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import {Route, Routes} from 'react-router-dom'
+import Header1 from './components/Header1';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+function App(){
+  // let arr = [34, "ayush", 343, {name: "golu", city:"bhopal"}, {name:"ayush", city: " bhopal"}]
+  // console.log(arr[4].name)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "text-center">
+      <Header1/>
+      <Routes>
+        <Route path = '/home' element={<Home/>} />
+        <Route path = '/About' element={<About/>} />
+        <Route path = '/Contact' element={<Contact/>} />
+
+      </Routes>
     </div>
   );
 }
